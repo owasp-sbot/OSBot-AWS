c93d0c1672dd12f0f5dc443d1750dae3dd0731d4
Dinis Cruz <dinis.cruz@owasp.org>
2019-04-11 21:22:21 +0100

    improved session tests

--- osbot_aws/apis/Session.py
+++ osbot_aws/apis/Session.py
@@ -1,13 +1,24 @@
 import boto3
+from botocore.session import get_session
 
 from osbot_aws.Globals import Globals
 
 
 class Session:
 
-    @staticmethod
-    def client(service_name):                               # todo: add caching to this call
-        profile_name = Globals.aws_session_profile_name
-        region_name  = Globals.aws_session_region_name
-        session = boto3.Session(profile_name=profile_name, region_name=region_name)
-        return session.client(service_name=service_name)
+    def client_boto3(self,service_name):
+        try:
+            profile_name = Globals.aws_session_profile_name
+            region_name  = Globals.aws_session_region_name
+
+            profiles = get_session()._build_profile_map()
+            if profile_name in profiles:
+                session = boto3.Session(profile_name=profile_name, region_name=region_name)
+                return {'status': 'ok', 'client': session.client(service_name=service_name) }
+            return { 'status' : 'ok', 'client': boto3.client(service_name=service_name)}
+        except Exception as error:
+            return {'status': 'error', 'data': '{0}'.format(error) }
+
+
+    def client(self, service_name):
+        return self.client_boto3(service_name).get('client')
