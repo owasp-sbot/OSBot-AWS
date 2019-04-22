b1513c71ee23261c76c20972c3ffd1193db00ec7
Dinis Cruz <dinis.cruz@owasp.org>
2019-04-22 16:01:43 +0100

    added Parameter SSM class

---
+++ osbot_aws/apis/Parameter.py
@@ -0,0 +1,81 @@
+import json
+
+from pbx_gs_python_utils.utils.Misc import Misc
+
+from osbot_aws.apis.Session import Session
+
+
+class Parameter:
+    def __init__(self, name=None):
+        self._ssm = None
+        self.name = name
+
+    # helpers
+    def ssm(self):
+        if self._ssm is None:
+            self._ssm = Session().client('ssm')
+        return self._ssm
+
+    # methods
+
+    def create(self,value):
+        return self.put(value)
+
+    def delete(self):
+        if self.exists() is False: return False
+        self.ssm().delete_parameter(Name=self.name)
+        return self.exists() is False
+
+    def exists(self):
+
+        return self.get() is not None
+
+    def get(self):
+        try:
+            return self.ssm().get_parameter(Name=self.name).get('Parameter')
+        except:
+            return None
+
+    def get_secret(self):
+        try:
+            return self.ssm().get_parameter(Name=self.name,WithDecryption=True).get('Parameter')
+        except:
+            return None
+
+    def list(self):
+        params = {}
+        for param in self.ssm().describe_parameters().get('Parameters'):
+            params[param.get('Name')] = param
+        return params
+
+    def push(self,data):
+        return self.put(json.dumps(data))
+
+    def pull(self):
+        try:
+            return json.loads(self.value())
+        except:
+            return None
+
+    def push_secret(self,data):
+        return self.put_secret(json.dumps(data))
+
+    def pull_secret(self):
+        try:
+            return json.loads(self.value_secret())
+        except:
+            return None
+
+    def put(self, value, description='', type='String',overwrite=True):
+        return self.ssm().put_parameter(Name=self.name, Value=value, Description=description,Type=type, Overwrite=overwrite)
+
+    def put_secret(self,value,description=''):
+        return self.put(value,description=description,type='SecureString')
+
+    def value(self):
+        return Misc.get_value(self.get(),'Value')
+
+    def value_secret(self):
+        return Misc.get_value(self.get_secret(),'Value')
+
+    def set_name(self,value): self.name = value ; return self
---
+++ tests/unit/apis/test_Parameter.py
@@ -0,0 +1,61 @@
+from unittest import TestCase
+
+from pbx_gs_python_utils.utils.Dev import Dev
+from pbx_gs_python_utils.utils.Misc import Misc
+
+from osbot_aws.apis.Parameter import Parameter
+
+
+class test_Parameters(TestCase):
+
+    def setUp(self):
+        self.name   = 'test_param'
+        self.value  = 'an value'
+        self.param = Parameter(self.name)
+        self.result = None
+        if self.param.exists() is False:            #  todo: move this to class methods
+            self.param.create(self.value)
+
+    def tearDown(self):
+        if self.result is not None:
+            Dev.pprint(self.result)
+        #self.param.delete()                         #  todo: move this to class methods
+
+    def test_ssm(self):
+        assert type(self.param.ssm()).__name__ == 'SSM'
+
+    def test_delete(self):
+        assert self.param.delete() is True
+
+    def test_list(self):
+        assert self.name in set(self.param.list())
+
+    def test_get(self):
+        assert self.param.set_name('aaa_bbbb').get() is None
+
+    def test_put(self):
+        value = Misc.random_string_and_numbers()
+        assert self.param.get().get('Type') =='String'
+        self.param.put(value)
+        assert self.param.value() == value
+
+    def test_push_pull(self):
+        data = {'answer' : 42}
+        self.param.push(data)
+        assert  self.param.pull() == data
+
+    def test_push_pull__secret(self):
+        data = {'answer is a secret' : 42}
+        self.param.push_secret(data)
+        assert  self.param.pull_secret() == data
+
+    def test_put_secret(self):
+        value = Misc.random_string_and_numbers()
+        self.param.put_secret(value)
+        assert value              == self.param.value_secret()
+        assert self.param.value() != self.param.value_secret()
+
+    def test_value(self):
+        self.result = self.param.value()
+
+
