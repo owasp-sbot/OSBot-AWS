847803912e750fbf364325e7846273686ab0c35a
Dinis Cruz <dinis.cruz@owasp.org>
2020-02-08 14:37:53 +0000

    Added more methods to API_Gateway class

--- osbot_aws/apis/API_Gateway.py
+++ osbot_aws/apis/API_Gateway.py
@@ -6,24 +6,61 @@ class API_Gateway:
     def __init__(self):
         self.api_gateway = Session().client('apigateway')
 
-    def _call_method_return_items(self, method_name, params={}, index_by='id'):
-        raw_data = getattr(self.api_gateway, method_name)(**params)
-        data = {}
-        for item in raw_data.get('items'):
-            data[item.get(index_by)] = item
-        return data
+    # helper methods
 
+    # see this tweet to understand the use of the 'data_key' param https://twitter.com/DinisCruz/status/1226113182240038912
+    # todo: add support for getting all the data using the position field
+    def _call_method_return_items(self, method_name, params={}, index_by='id', data_key='items'):
+        try:
+            raw_data = getattr(self.api_gateway, method_name)(**params)
+            data = {}
+            print('-----')
+            for item in raw_data.get(data_key):
+                #print(type(item), item)
+                data[item.get(index_by)] = item
+            return data
+        except Exception as error:
+            return {'error' : f'{error}'}
+
+    def _get(self, target, params={}, index_by='id'):
+        return self._call_method_return_items(method_name=f"get_{target}", params=params, index_by=index_by)
+
+    def _get_using_api_id(self, target, api_id, index_by='id', data_key='items'):
+        return self._call_method_return_items(method_name=f"get_{target}", params={'restApiId': api_id}, index_by=index_by, data_key=data_key)
+
+    # main methods
     def account(self):
         return self.api_gateway.get_account()
 
     def api_keys(self, index_by='id'):
         return self._call_method_return_items(method_name="get_api_keys", index_by=index_by)
 
-    def deployments(self, api_key):
-        return self._call_method_return_items(method_name="get_deployments", params={'restApiId':api_key})
+    def deployments(self, api_id):
+        return self._call_method_return_items(method_name="get_deployments", params={'restApiId':api_id})
 
     def domain_names(self):
         return self.api_gateway.get_domain_names().get('items')
 
+    def integration(self, api_id, resource_id, http_method):
+       return self._call_method_return_items('get_integration', params={'restApiId': api_id, 'resourceId': resource_id, 'httpMethod': http_method})
+
+    def method(self, api_id, resource_id, http_method):
+        return self._call_method_return_items('get_method', params={'restApiId': api_id, 'resourceId': resource_id, 'httpMethod': http_method})
+
+    def models(self, api_id):
+        return self._get_using_api_id('models', api_id)
+
+    def resources(self, api_id):
+        return self._get_using_api_id('resources', api_id)
+
     def rest_apis(self, index_by='id'):
         return self._call_method_return_items(method_name="get_rest_apis",index_by=index_by)
+
+    def stages(self, api_id):
+        return self._get_using_api_id('stages', api_id, index_by='deploymentId', data_key='item')
+
+    def usage(self, usage_plan_id, start_date, end_date):
+        return self._get('usage', params={'usagePlanId':usage_plan_id , 'startDate': start_date, 'endDate': end_date})
+
+    def usage_plans(self):
+        return self._get('usage_plans')
--- tests/unit/apis/test_API_Gateway.py
+++ tests/unit/apis/test_API_Gateway.py
@@ -6,8 +6,10 @@ class test_API_Gateway(Test_Helper):
 
     def setUp(self):
         super().setUp()
-        self.api_gateway  = API_Gateway()
-        self.test_api_id = 'xugc5ib648'  # VP-SaaS-Proxy
+        self.api_gateway        = API_Gateway()
+        self.test_api_id        = 'xugc5ib648'  # VP-SaaS-Proxy
+        self.test_resource_id   = 'lf80sgepa5'
+        self.test_usage_plan_id = '7a6fl9'
 
     def test__init__(self):
         assert type(self.api_gateway.api_gateway).__name__ == 'APIGateway'
@@ -25,13 +27,37 @@ class test_API_Gateway(Test_Helper):
         assert list(keys.values()).pop().get('enabled') == True
 
     def test_deployments(self):
-        self.result = self.api_gateway.deployments(self.test_api_id)
+        items = self.api_gateway.deployments(self.test_api_id)
+        assert len(items) > 2
 
     def test_domain_names(self):
         assert self.api_gateway.domain_names() == []
+
+    #not working: 'Invalid Method identifier specified'
+    def test_integration(self):
+       self.result = self.api_gateway.integration(self.test_api_id, self.test_resource_id, 'POST')
+
+    #not working: 'Invalid Method identifier specified'
+    def test_method(self):
+       self.result = self.api_gateway.method(self.test_api_id, self.test_resource_id, 'POST')
+
+    def test_models(self):
+        assert len(self.api_gateway.models(self.test_api_id)) > 1
+
+    def test_resources(self):
+        assert len(self.api_gateway.resources(self.test_api_id)) > 1
 
     def test_rest_apis(self):
         items = self.api_gateway.rest_apis(index_by='id')
         assert self.test_api_id in items
 
-        #self.result = self.api_gateway._call_method_return_items('get_rest_apis')#, {'restApiId': self.test_api_id})
+    def test_stages(self):
+        self.result = self.api_gateway.stages(self.test_api_id)
+
+    # def test_usage(self):
+    #     start_date ='2020-02-01'
+    #     end_date = '2020-02-10'
+    #     self.result = self.api_gateway.usage(self.test_usage_plan_id, start_date,end_date)
+
+    def test_usage_plans(self):
+        assert self.api_gateway.usage_plans().get(self.test_usage_plan_id).get('name') == '1k month'
