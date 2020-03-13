f996e7705e161c60ea07a2c189d9bbfd7fe50181
Dinis Cruz <dinis.cruz@owasp.org>
2020-03-13 18:13:30 +0000

    Minor fixes

--- osbot_aws/apis/test_helpers/Temp_S3_Zip_With_Lambda_File.py
+++ osbot_aws/apis/test_helpers/Temp_S3_Zip_With_Lambda_File.py
@@ -1,6 +1,6 @@
-from osbot_aws.Globals import Globals
-from osbot_utils.utils.Files import Files
-from osbot_utils.utils.Misc import random_string_and_numbers
+from osbot_aws.Globals       import Globals
+from osbot_utils.utils.Files import Files, file_create, path_combine, folder_create_temp
+from osbot_utils.utils.Misc  import random_string_and_numbers
 
 from osbot_aws.apis.S3 import S3
 
@@ -8,8 +8,8 @@ class Temp_S3_Zip_With_Lambda_File:
     def __init__(self, file_name=None,s3_bucket=None, s3_prefix=None):
         self.file_name    = file_name or f"temp_zip_file_{random_string_and_numbers()}"
         self.s3_bucket    = Globals.lambda_s3_bucket
-        self.s3_prefix    = f'{Globals.lambda_s3_key_prefix}/unit_tests/temp_zips/'
-        self.s3_key       = f'{self.s3_prefix}/{file_name}.zip'
+        self.s3_prefix    = f'{Globals.lambda_s3_key_prefix}/unit_tests/temp_zips'
+        self.s3_key       = f'{self.s3_prefix}/{self.file_name}.zip'
         self.folder       = None
         self.lambda_code  = "def run(event, context): return 'hello {0}'.format(event.get('name'))"
         self.tmp_file     = None
@@ -21,12 +21,14 @@ class Temp_S3_Zip_With_Lambda_File:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.delete()
 
+#    def create_s3_file(self, new_code = None):
+#
+
     def create_temp_file(self, new_code=None):
-        self.folder = Files.temp_folder('tmp_lambda_')
-        if new_code: self.lambda_code = new_code
-        self.tmp_file = Files.path_combine(self.folder, '{0}.py'.format(self.file_name))
-        Files.write(self.tmp_file, self.lambda_code)
-        assert Files.exists(self.tmp_file)
+        self.lambda_code = new_code or self.lambda_code
+        self.folder      = folder_create_temp('tmp_lambda_')
+        self.tmp_file    = path_combine(self.folder, f'{self.file_name}.py')
+        file_create(self.tmp_file, self.lambda_code)
         return self
 
     def delete(self):
