d472b8c078fac9c7fd2a2d162cdee5501b7d93dd
Dinis Cruz <dinis.cruz@owasp.org>
2020-12-08 07:29:30 +0000

    refactored Globals

--- osbot_aws/Globals.py
+++ osbot_aws/Globals.py
@@ -1,4 +1,6 @@
 import os
+
+from osbot_aws.AWS_Config import AWS_Config
 from osbot_aws.Config import Config
 
 # Globals vars
@@ -6,26 +8,36 @@ from osbot_aws.Config import Config
 # you can set them directly or via environment variables
 
 
-#todo: this neeeds to be refactored to be more compatible with the use of Env variables (see AWS_Config class)
 class Globals:
     config = Config()
 
-    default_values = {                                      # todo - refactor to better solution to store unit tests values (since these values shouldn't be hardcoded in the source code
-        'AWS_PROFILE_NAME'          : 'gw-bot'            ,
-        'AWS_REGION_NAME'           : 'eu-west-1'         ,
-        'AWS_ACCOUNT_ID'            : '311800962295'      ,
-        'OSBOT_LAMBDA_S3_BUCKET'    : 'gw-bot-lambdas'    ,
-        'OSBOT_LAMBDA_S3_KEY_PREFIX': 'lambdas'           ,
-        'OSBOT_LAMBDA_ROLE_NAME'    : 'gwbot-lambdas-temp',
-        'OSBOT_NAME'                : 'gw_bot'
-    }
+    # default_values = {                                      # todo - refactor to better solution to store unit tests values (since these values shouldn't be hardcoded in the source code
+    #     'AWS_PROFILE_NAME'          : 'gw-bot'            ,
+    #     'AWS_DEFAULT_REGION'        : 'eu-west-1'         ,
+    #     'AWS_ACCOUNT_ID'            : '311800962295'      ,
+    #     'OSBOT_LAMBDA_S3_BUCKET'    : 'gw-bot-lambdas'    ,
+    #     'OSBOT_LAMBDA_S3_KEY_PREFIX': 'lambdas'           ,
+    #     'OSBOT_LAMBDA_ROLE_NAME'    : 'gwbot-lambdas-temp',
+    #     'OSBOT_NAME'                : 'gw_bot'
+    # }
+    #
+    # aws_session_profile_name = os.getenv('AWS_PROFILE_NAME'             , default_values['AWS_PROFILE_NAME'          ])
+    # aws_session_region_name  = os.getenv('AWS_DEFAULT_REGION'           , default_values['AWS_DEFAULT_REGION'        ])
+    # aws_session_account_id   = os.getenv('AWS_ACCOUNT_ID'               , default_values['AWS_ACCOUNT_ID'            ])
+    # lambda_s3_bucket         = os.getenv('OSBOT_LAMBDA_S3_BUCKET'       , default_values['OSBOT_LAMBDA_S3_BUCKET'    ]) # must be unique in AWS
+    # lambda_s3_key_prefix     = os.getenv('OSBOT_LAMBDA_S3_KEY_PREFIX'   , default_values['OSBOT_LAMBDA_S3_KEY_PREFIX'])
+    # lambda_role_name         = os.getenv('OSBOT_LAMBDA_ROLE_NAME'       , default_values['OSBOT_LAMBDA_ROLE_NAME'    ])
+    # bot_name                 = os.getenv('OSBOT_NAME'                   , default_values['OSBOT_NAME'                ])
 
-    aws_session_profile_name = os.getenv('AWS_PROFILE_NAME'             , default_values['AWS_PROFILE_NAME'          ])
-    aws_session_region_name  = os.getenv('AWS_DEFAULT_REGION'           , default_values['AWS_DEFAULT_REGION'        ])
-    aws_session_account_id   = os.getenv('AWS_ACCOUNT_ID'               , default_values['AWS_ACCOUNT_ID'            ])
-    lambda_s3_bucket         = os.getenv('OSBOT_LAMBDA_S3_BUCKET'       , default_values['OSBOT_LAMBDA_S3_BUCKET'    ]) # must be unique in AWS
-    lambda_s3_key_prefix     = os.getenv('OSBOT_LAMBDA_S3_KEY_PREFIX'   , default_values['OSBOT_LAMBDA_S3_KEY_PREFIX'])
-    lambda_role_name         = os.getenv('OSBOT_LAMBDA_ROLE_NAME'       , default_values['OSBOT_LAMBDA_ROLE_NAME'    ])
-    bot_name                 = os.getenv('OSBOT_NAME'                   , default_values['OSBOT_NAME'                ])
+
+    aws_config = AWS_Config()
+
+    aws_session_profile_name    = aws_config.aws_session_profile_name()
+    aws_session_region_name     = aws_config.aws_session_region_name()
+    aws_session_account_id      = aws_config.aws_session_account_id()
+    lambda_s3_bucket            = aws_config.lambda_s3_bucket()
+    lambda_s3_key_prefix        = aws_config.lambda_s3_key_prefix()
+    lambda_role_name            = aws_config.lambda_role_name()
+    bot_name                    = aws_config.bot_name()
 
 
--- tests/test_Globals.py
+++ tests/test_Globals.py
@@ -4,20 +4,20 @@ from osbot_aws.Globals import Globals
 
 class test_Globals(TestCase):
 
-    def test_static_values(self):
-        assert Globals.aws_session_profile_name == 'gw-bot'
-        assert Globals.aws_session_region_name  == 'eu-west-1'
-        assert Globals.aws_session_account_id   == '311800962295'
-        assert Globals.bot_name                 == 'gw_bot'
-        assert Globals.lambda_s3_bucket         == 'gw-bot-lambdas'
-        assert Globals.lambda_s3_key_prefix     == 'lambdas'
-        assert Globals.lambda_role_name         == 'gwbot-lambdas-temp'
+    # def test_static_values(self):
+    #     assert Globals.aws_session_profile_name == 'gw-bot'
+    #     assert Globals.aws_session_region_name  == 'eu-west-1'
+    #     assert Globals.aws_session_account_id   == '311800962295'
+    #     assert Globals.bot_name                 == 'gw_bot'
+    #     assert Globals.lambda_s3_bucket         == 'gw-bot-lambdas'
+    #     assert Globals.lambda_s3_key_prefix     == 'lambdas'
+    #     assert Globals.lambda_role_name         == 'gwbot-lambdas-temp'
 
     def test_environment_variables(self):
-        assert Globals.aws_session_profile_name     == Globals.default_values['AWS_PROFILE_NAME'          ]
-        assert Globals.aws_session_region_name      == Globals.default_values['AWS_REGION_NAME'           ]
-        assert Globals.aws_session_account_id       == Globals.default_values['AWS_ACCOUNT_ID'            ]
-        assert Globals.lambda_s3_bucket             == Globals.default_values['OSBOT_LAMBDA_S3_BUCKET'    ]
-        assert Globals.lambda_s3_key_prefix         == Globals.default_values['OSBOT_LAMBDA_S3_KEY_PREFIX']
-        assert Globals.lambda_role_name             == Globals.default_values['OSBOT_LAMBDA_ROLE_NAME'    ]
-        assert Globals.bot_name                     == Globals.default_values['OSBOT_NAME'                ]
+        assert Globals.aws_session_profile_name     == os.getenv('AWS_PROFILE_NAME'          )
+        assert Globals.aws_session_region_name      == os.getenv('AWS_DEFAULT_REGION'        )
+        assert Globals.aws_session_account_id       == os.getenv('AWS_ACCOUNT_ID'            )
+        assert Globals.lambda_s3_bucket             == os.getenv('OSBOT_LAMBDA_S3_BUCKET'    )
+        assert Globals.lambda_s3_key_prefix         == os.getenv('OSBOT_LAMBDA_S3_KEY_PREFIX')
+        assert Globals.lambda_role_name             == os.getenv('OSBOT_LAMBDA_ROLE_NAME'    )
+        assert Globals.bot_name                     == os.getenv('OSBOT_NAME'                )
