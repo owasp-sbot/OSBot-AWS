e07c0f785177f59ce955b65591da4c5db1c4097e
dinis-cruz-gw <57914593+dinis-cruz-gw@users.noreply.github.com>
2021-01-20 01:21:40 +0000

    Update Ec2.py

--- osbot_aws/apis/Ec2.py
+++ osbot_aws/apis/Ec2.py
@@ -6,7 +6,7 @@ from osbot_aws.apis.Session import Session
 class Ec2:
     def __init__(self):
         self.ec2          = Session().client('ec2')
-        self.ec2_resource = boto3.resource('ec2')
+        self.ec2_resource = Session().resource('ec2')
 
 
 
