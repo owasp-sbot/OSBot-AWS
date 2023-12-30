d7a62dd3684b716f9a1b3117ceeba9c9e0f6d721
Dinis Cruz <dinis.cruz@owasp.org>
2023-12-30 19:32:43 +0000

    Started adding EC_Cluster class

---
+++ tests/integration/aws/ecs/test_ECS_Cluster.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+from osbot_utils.utils.Dev import pprint
+
+from osbot_aws.aws.ecs.ECS import ECS
+from osbot_aws.aws.ecs.ECS_Cluster import ECS_Cluster
+
+
+class test_ECS_Cluster(TestCase):
+
+    def setUp(self):
+        self.ecs_cluster = ECS_Cluster()
+
+    def test__init__(self):
+        assert type(self.ecs_cluster.ecs) is ECS
+        assert self.ecs_cluster.cluster_name.startswith('test_ecs_cluster_')
