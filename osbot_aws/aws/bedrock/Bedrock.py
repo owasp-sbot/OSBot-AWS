80b276807cd96813d77b934dc5e05addc14b8f17
Dinis Cruz <dinis.cruz@owasp.org>
2024-02-24 01:05:10 +0000

    added list of supported models test to test_Bedrock

--- osbot_aws/aws/bedrock/Bedrock.py
+++ osbot_aws/aws/bedrock/Bedrock.py
@@ -4,7 +4,10 @@ from botocore.exceptions import ClientError
 
 from osbot_aws.apis.Session import Session
 from osbot_utils.base_classes.Kwargs_To_Self import Kwargs_To_Self
+from osbot_utils.decorators.lists.group_by import group_by
+from osbot_utils.decorators.lists.index_by import index_by
 from osbot_utils.decorators.methods.cache_on_self import cache_on_self
+from osbot_utils.utils.Dev import pprint
 
 
 class Bedrock(Kwargs_To_Self):
@@ -18,8 +21,26 @@ class Bedrock(Kwargs_To_Self):
         session = Session(region_name=self.region_name)
         return session.client('bedrock')
 
+    @index_by
+    @group_by
     def models(self):
         response = self.client().list_foundation_models()
-        return response
-        models = response["modelSummaries"]
+        models   = response.get("modelSummaries")
+        return models
+
+    def models_active(self):
+        models = {}
+        for model in self.models():
+            model_id = model.get("modelId")
+            provider = model.get("providerName")
+            output   = model.get('outputModalities').pop()   # note: assuming that there is only one output per model
+            assert len(model_id.split( ".")) == 2
+
+            status   = model.get('modelLifecycle').get('status')
+            if status == 'ACTIVE':
+                providers = models   [provider] = models   .get(provider, {})
+                outputs   = providers[output  ] = providers.get(output  , {})
+                outputs[model_id] = model
+            # pprint(model)
+            # break
         return models
--- tests/integration/aws/bedrock/test_Bedrock.py
+++ tests/integration/aws/bedrock/test_Bedrock.py
@@ -11,6 +11,7 @@ from osbot_aws.aws.iam.IAM_Assume_Role import IAM_Assume_Role
 from osbot_utils.decorators.methods.cache_on_self import cache_on_self
 from osbot_utils.testing.Patch_Print import Patch_Print
 from osbot_utils.utils.Dev import pprint
+from osbot_utils.utils.Lists import list_contains_list
 from osbot_utils.utils.Misc import list_set
 from osbot_utils.utils.Objects import type_full_name, obj_info
 
@@ -32,16 +33,16 @@ class Bedrock__with_temp_role(Bedrock):
 class test_Bedrock(TestCase):
 
     def setUp(self):
-        self.region_name = 'eu-central-1'
+        self.region_name = 'us-east-1'
         self.bedrock     = Bedrock__with_temp_role(region_name=self.region_name)
 
     def test___init__(self):
-        assert type_full_name(self.bedrock        ) == 'osbot_aws.aws.bedrock.Bedrock.Bedrock'
+        assert type_full_name(self.bedrock        ) == 'test_Bedrock.Bedrock__with_temp_role'
 
     def test_client(self):
         client = self.bedrock.client()
-        assert type_full_name(client) == 'botocore.client.Bedrock'
-        assert client.meta.region_name  == 'eu-central-1'
+        assert type_full_name(client)                      == 'botocore.client.Bedrock'
+        assert client.meta.region_name                     == 'us-east-1'
         assert list_set(client.meta.method_to_api_mapping) == [ 'create_model_customization_job'                ,
                                                                 'create_provisioned_model_throughput'           ,
                                                                 'delete_custom_model'                           ,
@@ -65,4 +66,49 @@ class test_Bedrock(TestCase):
 
     #@capture_iam_exception
     def test_models(self):
-        self.bedrock.models()
+        expected_attributes = ['customizationsSupported', 'inferenceTypesSupported', 'inputModalities', 'modelArn',
+                               'modelId', 'modelLifecycle', 'modelName', 'outputModalities',
+                               'providerName', ] #'responseStreamingSupported'
+        models = self.bedrock.models()
+        for model  in models:
+            assert list_contains_list(list_set(model), expected_attributes)
+            assert list_set(model.get('modelLifecycle')) == ['status']
+        assert len(models) == 45
+
+    def test_models_active(self):
+        models                     = self.bedrock.models_active()
+        models_a1_labs             = models.get('AI21 Labs'   )
+        models_a1_labs__text       = models.get('AI21 Labs'   ).get('TEXT')
+        models_amazon              = models.get('Amazon'      )
+        models_amazon__embedding   = models.get('Amazon'      ).get('EMBEDDING')
+        models_amazon__image       = models.get('Amazon'      ).get('IMAGE'    )
+        models_amazon__text        = models.get('Amazon'      ).get('TEXT')
+        models_anthropic           = models.get('Anthropic'   )
+        models_anthropic__text     = models.get('Anthropic'   ).get('TEXT')
+        models_cohere              = models.get('Cohere'      )
+        models_cohere___text       = models.get('Cohere'      ).get('TEXT')
+        models_cohere___embedding  = models.get('Cohere'      ).get('EMBEDDING')
+        models_meta                = models.get('Meta'        )
+        models_meta__text          = models.get('Meta'        ).get('TEXT')
+        models_stability_ai        = models.get('Stability AI')
+        models_stability_ai__image = models.get('Stability AI').get("IMAGE")
+
+
+        assert list_set(models                    ) == ['AI21 Labs', 'Amazon', 'Anthropic', 'Cohere', 'Meta', 'Stability AI']
+        assert list_set(models_a1_labs            ) == ['TEXT'                         ]
+        assert list_set(models_amazon             ) == ['EMBEDDING', 'IMAGE', 'TEXT'   ]
+        assert list_set(models_anthropic          ) == ['TEXT'                         ]
+        assert list_set(models_cohere             ) == ['EMBEDDING', 'TEXT'            ]
+        assert list_set(models_meta               ) == ['TEXT'                         ]
+        assert list_set(models_stability_ai       ) == ['IMAGE'                        ]
+        assert list_set(models_a1_labs__text      ) == ['ai21.j2-grande-instruct'         , 'ai21.j2-jumbo-instruct'            , 'ai21.j2-mid'                  , 'ai21.j2-mid-v1'                , 'ai21.j2-ultra'                  , 'ai21.j2-ultra-v1'                                                                       ]
+        assert list_set(models_amazon__embedding  ) == ['amazon.titan-embed-g1-text-02'   , 'amazon.titan-embed-image-v1'       , 'amazon.titan-embed-image-v1:0', 'amazon.titan-embed-text-v1'    , 'amazon.titan-embed-text-v1:2:8k'                                                                                           ]
+        assert list_set(models_amazon__image      ) == ['amazon.titan-image-generator-v1' , 'amazon.titan-image-generator-v1:0'                                                                                                                                                                                                  ]
+        assert list_set(models_amazon__text       ) == ['amazon.titan-text-express-v1'    , 'amazon.titan-text-express-v1:0:8k' , 'amazon.titan-text-lite-v1'    , 'amazon.titan-text-lite-v1:0:4k', 'amazon.titan-tg1-large'                                                                                                    ]
+        assert list_set(models_anthropic__text    ) == ['anthropic.claude-instant-v1'     , 'anthropic.claude-instant-v1:2:100k', 'anthropic.claude-v2'          , 'anthropic.claude-v2:0:100k'    , 'anthropic.claude-v2:0:18k'      , 'anthropic.claude-v2:1'       , 'anthropic.claude-v2:1:18k', 'anthropic.claude-v2:1:200k']
+        assert list_set(models_cohere___text      ) == ['cohere.command-light-text-v14'   , 'cohere.command-light-text-v14:7:4k', 'cohere.command-text-v14'      , 'cohere.command-text-v14:7:4k'                                                                                                                                ]
+        assert list_set(models_cohere___embedding ) == ['cohere.embed-english-v3'         , 'cohere.embed-multilingual-v3'                                                                                                                                                                                                       ]
+        assert list_set(models_meta__text         ) == ['meta.llama2-13b-chat-v1'         , 'meta.llama2-13b-chat-v1:0:4k'      , 'meta.llama2-13b-v1'           , 'meta.llama2-13b-v1:0:4k'       , 'meta.llama2-70b-chat-v1'        , 'meta.llama2-70b-chat-v1:0:4k', 'meta.llama2-70b-v1'       , 'meta.llama2-70b-v1:0:4k'   ]
+        assert list_set(models_stability_ai__image) == ['stability.stable-diffusion-xl-v1', 'stability.stable-diffusion-xl-v1:0'                                                                                                                                                                                                 ]
+
+
