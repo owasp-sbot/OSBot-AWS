acf218e1822e606f2c3d43a2c2224795c8219f8e
Dinis Cruz <dinis.cruz@owasp.org>
2024-04-03 15:45:23 +0100

    added assert

--- tests/integration/aws/bedrock/cache/test_Bedrock_Cache__Html_With_Images.py
+++ tests/integration/aws/bedrock/cache/test_Bedrock_Cache__Html_With_Images.py
@@ -3,6 +3,7 @@ from unittest import TestCase
 from osbot_aws.aws.bedrock.cache.Bedrock_Cache__Html_With_Images import Bedrock_Cache__Html_With_Images
 from osbot_aws.aws.bedrock.cache.Bedrock__Cache import Bedrock__Cache
 from osbot_utils.helpers.html.Tag__Div import Tag__Div
+from osbot_utils.utils.Files import file_exists
 
 
 class test_Bedrock_Cache__Html_With_Images(TestCase):
@@ -13,5 +14,6 @@ class test_Bedrock_Cache__Html_With_Images(TestCase):
     def test_create(self):
         model_id = 'amazon.titan-image-generator-v1'
         self.html_with_images.create(model_id = model_id)
+        assert file_exists(self.html_with_images.target_file)
 
 
