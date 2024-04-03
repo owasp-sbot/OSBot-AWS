a784599ab1c2613f87ef52c599460d422ed6de8d
Dinis Cruz <dinis.cruz@owasp.org>
2024-04-03 14:37:10 +0100

    improved use of Tag__* in test_Amazon_Titan_Image_Generator

--- tests/integration/aws/bedrock/models/amazon/test_Amazon_Titan_Image_Generator_V1.py
+++ tests/integration/aws/bedrock/models/amazon/test_Amazon_Titan_Image_Generator_V1.py
@@ -80,11 +80,9 @@ class test_Amazon_Titan_Image_Generator_V1(TestCase__Bedrock):
         assert error is None
         assert len(self.png_data) > 30000        # was first 513424 then 520564
 
-    #@pytest.mark.skip(reason="not implemented")
     def test__see_images_in_cache(self):
-
         div_test_image = Tag__Div()
-        div_images = [div_test_image]
+        div_images = []
 
         for request_data in self.cache.requests_data__with_model_id(self.model.model_id):
             request_hash        = request_data.get('_hash')
@@ -94,21 +92,21 @@ class test_Amazon_Titan_Image_Generator_V1(TestCase__Bedrock):
             image_params        = request_data.get('body').get('textToImageParams')
             image_text          = image_params.get('text')
             image_negative_text = image_params.get('negativeText', "")
-            response_data = self.cache.response_data_for__request_hash(request_hash)
-            images        = response_data.get('images')
-            image_base64  = images[0]
-            image_html_code = f"""<div class="">
-                                    <div class="badge bg-primary">Cache item #{request_id}</div>
-                                    <hr/>                                    
-                                    <img class="base64-image img-fluid" src="data:image/jpeg;base64,{image_base64}">
-                                    <div class="var_name">Text:</div>
-                                    <div><strong>{image_text}</strong></div>
-                                    <hr/>
-                                    <div class="var_name">Negative Text</div> 
-                                    <div><strong>{image_negative_text}</strong></div>
-                                  </div>"""
-            div_image = Tag__Div(inner_html  = image_html_code                   ,
-                                 tag_classes = ['col', 'col-md-3', 'text-center'])
+            response_data       = self.cache.response_data_for__request_hash(request_hash)
+            images              = response_data.get('images')
+            image_base64        = images[0]
+
+            div_image = Tag__Div(tag_classes = ['col', 'col-md-3', 'text-center'])
+            div_badge          = Tag__Div(tag_classes=['badge','bg-primary'], inner_html ="Cache item #0")
+            hr                 = Tag__HR()
+            div_text_title     = Tag__Div(tag_classes=['var_name'], inner_html="Text:")
+            div_text_value     = Tag__Div(inner_html=f"<strong>{image_text}</strong>")
+            div_negative_title = Tag__Div(tag_classes=['var_name'], inner_html="Negative Text:")
+            div_negative_value = Tag__Div(inner_html=f"<strong>{image_negative_text}</strong>")
+
+            img                = Tag__Base(tag_name="img", tag_classes=['base64-image','img-fluid'])
+            img.attributes['src'] = f"data:image/jpeg;base64,{image_base64}"
+            div_image.append(div_badge, hr, img, div_text_title, div_text_value, div_negative_title, div_negative_value)
             div_images.append(div_image)
 
         self.create_temp_html_file_with_images(div_images)
@@ -133,11 +131,12 @@ class test_Amazon_Titan_Image_Generator_V1(TestCase__Bedrock):
                                     padding:10px }
                     .bg-dark { font-size:15px }
                     .var_name { font-size:12px }"""
-        link_bootstrap = Tag__Link(href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css", rel="stylesheet")
 
+        #link_bootstrap = Tag__Link(href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css", rel="stylesheet")
         head_style     = Tag__Base(tag_name='style', inner_html = css_style)
-        head_tag       = Tag__Head(elements= [head_style, link_bootstrap])
+        head_tag       = Tag__Head(elements= [head_style])
         head_tag.title = text_title
+        head_tag.add_css_bootstrap()
         html_tag       = Tag__Html(head=head_tag)
         html_tag.body.append(div_container )
 
