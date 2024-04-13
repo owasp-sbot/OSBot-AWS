8aa75293cb9e2fba78a84649d065951773e99d5e
Dinis Cruz <dinis.cruz@owasp.org>
2024-04-13 13:22:59 +0100

    refactored out code from Bedrock__Cache into OSBot_Utils Sqlite__Cache__Requests

--- osbot_aws/aws/bedrock/cache/Bedrock__Cache.py
+++ osbot_aws/aws/bedrock/cache/Bedrock__Cache.py
@@ -19,81 +19,22 @@ SQLITE_TABLE__BEDROCK_REQUESTS = 'bedrock_requests'
 class Bedrock__Cache(Sqlite__Cache__Requests):
     db_name        : str
     table_name     : str
-    sqlite_bedrock : Sqlite__DB__Requests = None
 
     def __init__(self, db_path=None):
         load_dotenv()
-        super().__init__()
         self.db_name    = environ.get(ENV_NAME_BEDROCK_DB_NAME) or SQLITE_DB_NAME__SQLITE_BEDROCK
         self.table_name = SQLITE_TABLE__BEDROCK_REQUESTS
-        self.sqlite_bedrock = Sqlite__DB__Requests(db_path=db_path, db_name=self.db_name, table_name=self.table_name)
+        super().__init__(db_path=db_path, db_name=self.db_name, table_name=self.table_name)
 
-    def cache_add(self, request_data, response_data):
-        new_row_obj = self.create_new_cache_obj(request_data, response_data)
-        return self.cache_table().row_add_and_commit(new_row_obj)
 
-    def cache_delete(self, request_data):
-        request_data        = json_dumps(request_data)
-        request_data_sha256 = str_sha256(request_data)
-        return self.cache_table().rows_delete_where(request_hash=request_data_sha256)
 
-    def cache_entries(self):
-        return self.cache_table().rows()
 
-    def cache_entry(self, request_data):
-        request_data        = json_dumps(request_data)
-        request_data_sha256 = str_sha256(request_data)
-        data                = self.cache_table().select_rows_where(request_hash=request_data_sha256)
-        if len(data) > 0:                       # todo: add logic to handle (or log), where there are multiple entries with the same hash
-            return data[0]
-        return {}
 
-    def cache_entry_comments(self, model_id, body):
-        cache_entry = self.cache_entry_for_request_params(model_id=model_id, body=body)
-        return cache_entry.get('comments')
 
-    def cache_entry_comments_update(self, model_id, body, new_comments):
-        cache_entry      = self.cache_entry_for_request_params(model_id=model_id, body=body)
-        request_hash     = cache_entry.get('request_hash')
-        update_fields    = dict(comments=new_comments)
-        query_conditions = dict(request_hash=request_hash)
-        result           = self.cache_table().row_update(update_fields, query_conditions)
-        return result
 
-    def cache_entry_for_request_params(self, **target_kwargs):
-        request_data = self.cache_request_data(**target_kwargs)
-        return self.cache_entry(request_data)
 
-    def create_new_cache_data(self, request_data, response_data):
-        request_data_json  = json_dumps(request_data)
-        request_data_hash  = str_sha256(request_data_json)
-        response_data_json = json_dumps(response_data)
-        response_data_hash = str_sha256(response_data_json)
-        if self.add_timestamp:
-            timestamp = timestamp_utc_now()
-        else:
-            timestamp = 0
-        return dict(request_data  = request_data_json   ,
-                    request_hash  = request_data_hash   ,
-                    response_data = response_data_json  ,
-                    response_hash = response_data_hash  ,
-                    timestamp     = timestamp           )
 
-    def create_new_cache_obj(self, request_data, response_data):
-        new_row_data = self.create_new_cache_data(request_data, response_data)
-        new_row_obj = self.cache_table().new_row_obj(new_row_data)
-        return new_row_obj
 
-    def cache_table(self):
-        return self.sqlite_bedrock.table_requests()
-
-    def cache_table__clear(self):
-        return self.cache_table().clear()
-
-    def cache_request_data(self, model_id, body):
-        request_data = dict(model        = model_id     ,
-                            body         = body         )
-        return request_data
 
     def comments(self, model):
         model_id = model.model_id
@@ -119,95 +60,6 @@ class Bedrock__Cache(Sqlite__Cache__Requests):
         model_id = model.model_id
         body     = model.body()
         return self.cache_entry_comments_update(model_id=model_id, body=body, new_comments=new_comments)
-
-    def delete_where_request_data(self, request_data):                                      # todo: check if it is ok to use the request_data as a query target, or if we should use the request_hash variable
-        if type(request_data) is dict:                                                      # if we get an request_data obj
-            request_data = json_dumps(request_data)                                         # convert it to the json dump
-        if type(request_data) is str:                                                       # make sure we have a string
-            if len(self.rows_where__request_data(request_data)) > 0:                        # make sure there is at least one entry to delete
-                self.cache_table().rows_delete_where(request_data=request_data)             # delete it
-                return len(self.rows_where__request_data(request_data)) == 0                # confirm it was deleted
-        return False                                                                        # if anthing was not right, return False
-
-    def disable(self):
-        self.enabled = False
-        return self
-
-    def enable(self):
-        self.enabled = True
-        return self
-
-    def invoke_with_cache(self, target,target_kwargs, request_data=None):
-        if self.enabled is False:
-            if self.cache_only_mode:
-                return None
-            return target(**target_kwargs)
-        if request_data is None:
-            request_data  = self.cache_request_data(**target_kwargs)
-        cache_entry   = self.cache_entry(request_data)
-        if cache_entry:
-            if self.update_mode is True:
-                self.cache_delete(request_data)
-            else:
-                response_data_json = cache_entry.get('response_data')
-                if response_data_json:
-                    return json_loads(response_data_json)
-        if self.cache_only_mode is False:
-            response_data = target(**target_kwargs)
-            self.cache_add(request_data=request_data, response_data=response_data)
-            return response_data
-
-    def only_from_cache(self, value=True):
-        self.cache_only_mode = value
-        return self
-
-    def response_data_for__request_hash(self, request_hash):
-        rows = self.rows_where__request_hash(request_hash)
-        if len(rows) > 0:
-            row = rows[0]
-            response_data     = row.get('response_data')
-            response_data_obj = json_loads(response_data)
-            return response_data_obj
-        return {}
-
-    def requests_data__all(self):
-        requests_data = []
-        for row in self.cache_table().rows():
-            req_id           = row.get('id')
-            request_data     = row.get('request_data')
-            request_hash     = row.get('request_hash')
-            request_comments = row.get('comments')
-            request_data_obj = json_loads(request_data)
-            request_data_obj['_id'      ] = req_id
-            request_data_obj['_hash'    ] = request_hash
-            request_data_obj['_comments'] = request_comments
-
-            requests_data.append(request_data_obj)
-        return requests_data
-
-    def requests_data__by_model_id(self):
-        values        = self.requests_data__all()
-        group_by      = 'model'
-        requests_data = list_group_by(values=values, group_by=group_by)
-        if 'None' in requests_data:                         # if there are other entries in the cache
-            del requests_data['None']                       # don't include them
-        return requests_data
-
-    def requests_data__with_model_id(self, model_id):
-        return self.requests_data__by_model_id().get(model_id, [])
-
-    def rows_where(self, **kwargs):
-        return self.cache_table().select_rows_where(**kwargs)
-
-    def rows_where__request_data(self, request_data):
-        return self.rows_where(request_data=request_data)
-
-    def rows_where__request_hash(self, request_hash):
-        return self.rows_where(request_hash=request_hash)
-
-    def update(self, value=True):
-        self.update_mode = value
-        return self
 
     # Bedrock overwrite methods
 
