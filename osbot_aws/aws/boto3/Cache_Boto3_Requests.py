75e6a8abdb804a04fb616f327c07794df4cc1cc6
Dinis Cruz <dinis.cruz@owasp.org>
2024-04-15 15:55:33 +0100

    minor improvement to Cache_Boto3_Requests

--- osbot_aws/aws/boto3/Cache_Boto3_Requests.py
+++ osbot_aws/aws/boto3/Cache_Boto3_Requests.py
@@ -6,13 +6,16 @@ from osbot_utils.helpers.sqlite.domains.Sqlite__Cache__Requests__Patch import Sq
 from osbot_utils.utils.Dev import pprint
 from osbot_utils.utils.Yaml import yaml_parse, yaml_to_str
 
+SQLITE_DB_NAME__BOTO3_REQUESTS_CACHE = 'boto3_requests_cache.sqlite'
 
 class Cache_Boto3_Requests(Sqlite__Cache__Requests__Patch):
-    def __init__(self):
+
+    def __init__(self, db_path=None):
         self.target_class           = BaseClient
         self.target_function        = BaseClient._make_api_call
         self.target_function_name   = "_make_api_call"
-        super().__init__()
+        self.db_name                = SQLITE_DB_NAME__BOTO3_REQUESTS_CACHE
+        super().__init__(db_path=db_path)
 
     def invoke_target(self, target, target_args, target_kwargs):
         #cached_response = {'Account': '470426667096'}
--- tests/integration/aws/boto3/test_Cache_Boto3_Requests.py
+++ tests/integration/aws/boto3/test_Cache_Boto3_Requests.py
@@ -1,24 +1,31 @@
+from os import environ
 from unittest import TestCase
 
 from botocore.client import BaseClient
+from dotenv import load_dotenv
 
-from osbot_aws.aws.boto3.Cache_Boto3_Requests import Cache_Boto3_Requests
+from osbot_aws.aws.boto3.Cache_Boto3_Requests import Cache_Boto3_Requests, SQLITE_DB_NAME__BOTO3_REQUESTS_CACHE
 from osbot_aws.aws.boto3.View_Boto3_Rest_Calls import print_boto3_calls
 from osbot_aws.aws.iam.STS import STS
 from osbot_utils.base_classes.Kwargs_To_Self import Kwargs_To_Self
 from osbot_utils.helpers.sqlite.domains.Sqlite__Cache__Requests import Sqlite__Cache__Requests
 from osbot_utils.helpers.sqlite.domains.Sqlite__Cache__Requests__Patch import Sqlite__Cache__Requests__Patch
+from osbot_utils.helpers.sqlite.domains.Sqlite__DB__Local import ENV_NAME_PATH_LOCAL_DBS
 from osbot_utils.utils.Dev import pprint
-from osbot_utils.utils.Files import current_temp_folder, parent_folder, file_name
+from osbot_utils.utils.Files import current_temp_folder, parent_folder, file_name, temp_file, file_extension, \
+    folder_exists
 from osbot_utils.utils.Objects import base_types
 
 
 class test_Cache_Boto3_Requests(TestCase):
     cache_boto3_requests : Cache_Boto3_Requests
+    temp_db_path         : str
 
     @classmethod
     def setUpClass(cls):
-        cls.cache_boto3_requests = Cache_Boto3_Requests()
+        load_dotenv()
+        cls.temp_db_path = temp_file(extension='sqlite')
+        cls.cache_boto3_requests = Cache_Boto3_Requests(db_path=cls.temp_db_path)
 
     @classmethod
     def tearDownClass(cls):
@@ -28,13 +35,13 @@ class test_Cache_Boto3_Requests(TestCase):
     def test__init__(self):
         _ = self.cache_boto3_requests           # we can't use the with context here since it auto applies the patch
 
-        assert _.__attr_names__() == ['add_timestamp', 'cache_only_mode', 'db_name','enabled','on_invoke_target',
-                                      'pickle_response', 'sqlite_requests','table_name','target_class',
-                                      'target_function', 'target_function_name','update_mode']
-        assert _.db_name   .startswith('requests_cache_')
+        assert _.__attr_names__()                         == ['add_timestamp', 'cache_only_mode', 'db_name','enabled','on_invoke_target',
+                                                              'pickle_response', 'sqlite_requests','table_name','target_class',
+                                                              'target_function', 'target_function_name','update_mode']
+        assert _.db_name                                  == SQLITE_DB_NAME__BOTO3_REQUESTS_CACHE
         assert _.table_name.startswith('requests_table_')
-        assert _.sqlite_requests.exists() is True
-        assert _.cache_entries() == []
+        assert _.sqlite_requests.exists()                 is True
+        assert _.cache_entries()                          == []
         assert _.cache_table().new_row_obj().__locals__() == {'cache_hits'      : 0     ,
                                                               'comments'        : ''    ,
                                                               'latest'          : False ,
@@ -44,15 +51,15 @@ class test_Cache_Boto3_Requests(TestCase):
                                                               'response_data'   : ''    ,
                                                               'response_hash'   : ''    ,
                                                               'timestamp'       : 0     }
-        assert parent_folder(_.sqlite_requests.db_path) == current_temp_folder()
-        assert file_name    (_.sqlite_requests.db_path).startswith('requests_cache_')
-        assert base_types(_) == [Sqlite__Cache__Requests__Patch ,
-                                 Sqlite__Cache__Requests        ,
-                                 Kwargs_To_Self                 ,
-                                 object                         ]
-        assert _.target_class           == BaseClient
-        assert _.target_function        == BaseClient._make_api_call
-        assert _.target_function_name   == "_make_api_call"
+        assert parent_folder (_.sqlite_requests.db_path)  == current_temp_folder()
+        assert file_extension(_.sqlite_requests.db_path)  == '.sqlite'
+        assert base_types(_)                              == [Sqlite__Cache__Requests__Patch ,
+                                                              Sqlite__Cache__Requests        ,
+                                                              Kwargs_To_Self                 ,
+                                                              object                         ]
+        assert _.target_class                             == BaseClient
+        assert _.target_function                          == BaseClient._make_api_call
+        assert _.target_function_name                     == "_make_api_call"
 
     def test___enter____exit__(self):
         assert BaseClient._make_api_call == BaseClient._make_api_call
@@ -100,3 +107,10 @@ class test_Cache_Boto3_Requests(TestCase):
         sts = STS()
         sts.current_account_id()
 
+class test_Cache_Boto3_Requests__Local_DBs(TestCase):
+
+    def test__local_dbs_with_env_value_set(self):
+        load_dotenv()
+        path_local_dbs = environ.get(ENV_NAME_PATH_LOCAL_DBS)
+        if path_local_dbs:
+            assert folder_exists(path_local_dbs)
