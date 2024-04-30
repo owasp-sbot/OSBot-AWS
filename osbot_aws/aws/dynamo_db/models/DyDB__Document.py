96947c421cfd45db78db46b0724f3f93073c4a9a
Dinis Cruz <dinis.cruz@owasp.org>
2024-05-01 00:11:47 +0100

    added more methods to DyDB__Document

--- osbot_aws/aws/dynamo_db/models/DyDB__Document.py
+++ osbot_aws/aws/dynamo_db/models/DyDB__Document.py
@@ -1,8 +1,9 @@
-from boto3.dynamodb.types                               import TypeSerializer
+from boto3.dynamodb.types import TypeSerializer, TypeDeserializer
 from osbot_aws.aws.dynamo_db.domains.DyDB__Table        import DyDB__Table
 from osbot_utils.base_classes.Kwargs_To_Self            import Kwargs_To_Self
 from osbot_utils.decorators.methods.cache_on_self       import cache_on_self
 from osbot_utils.decorators.methods.remove_return_value import remove_return_value
+from osbot_utils.utils.Dev import pprint
 from osbot_utils.utils.Json                             import to_json_str
 from osbot_utils.utils.Misc                             import list_set
 
@@ -14,18 +15,7 @@ class DyDB__Document(Kwargs_To_Self):
     def __repr__(self):
         return f'[DyDB__Document]\n' + to_json_str(self.document)
 
-    @remove_return_value(field_name='ResponseMetadata')
-    def add_field(self, field_name, field_value):
-
-        kwargs = dict(TableName                 = self.table.table_name                         ,
-                      Key                       = self.exp_key()                                ,
-                      UpdateExpression          = 'SET #field_name = :field_value'              ,
-                      ExpressionAttributeNames  = { '#field_name': field_name                   },
-                      ExpressionAttributeValues = { ':field_value': self.serialize(field_value) },
-                      ReturnValues              = 'UPDATED_NEW'                                 )
-
-        response = self.client().update_item(**kwargs)
-        return response
+    # support methods
 
     def client(self):
         return self.table.dynamo_db.client()
@@ -37,7 +27,7 @@ class DyDB__Document(Kwargs_To_Self):
         return list_set(self.document)
 
     def exp_key(self):
-        return {self.key_name(): self.serialize(self.key_value()) }
+        return {self.key_name(): self.serialize_value(self.key_value()) }
 
     def key_name(self):
         return self.table.key_name
@@ -49,10 +39,57 @@ class DyDB__Document(Kwargs_To_Self):
         self.document = self.table.document(self.document_id())
         return self.document
 
-    def serialize(self, value):
+    def serialize_value(self, value):
         return self.type_serializer().serialize(value)
+
+    def deserialize_value(self, value):
+        return self.type_deserializer().deserialize(value)
+
+    @cache_on_self
+    def type_deserializer(self):
+        return TypeDeserializer()
 
     @cache_on_self
     def type_serializer(self):
         return TypeSerializer()
 
+    # action methods
+
+    @remove_return_value(field_name='ResponseMetadata')
+    def add_field(self, field_name, field_value):
+
+        kwargs = dict(TableName                 = self.table.table_name                                ,
+                      Key                       = self.exp_key()                                       ,
+                      UpdateExpression          = 'SET #field_name = :field_value'                     ,
+                      ExpressionAttributeNames  = { '#field_name': field_name                         },
+                      ExpressionAttributeValues = { ':field_value': self.serialize_value(field_value) },
+                      ReturnValues              = 'UPDATED_NEW'                                       )
+
+        response = self.client().update_item(**kwargs)
+        return response.get('Attributes')
+
+    def add_to_list(self, list_field_name, new_list_element):
+        kwargs = dict( TableName        = self.table.table_name,
+                       Key              = self.exp_key(),
+                       UpdateExpression = f"SET {list_field_name} = list_append(if_not_exists({list_field_name}, :empty_list), :new_element)",
+                       ExpressionAttributeValues={ ':new_element': self.serialize_value([new_list_element]),    # new element must be wrapped in a list
+                                                   ':empty_list' : self.serialize_value([])                     # default empty list if the list does not exist
+            },
+            ReturnValues='UPDATED_NEW'  # Returns the updated list
+        )
+        response = self.client().update_item(**kwargs)
+        return response.get('Attributes')
+
+    def delete_field(self, field_name):
+        if field_name not in self.fields():
+            return False
+        kwargs = dict( TableName                 = self.table.table_name      ,
+                        Key                      = self.exp_key()             ,
+                        UpdateExpression         = f'REMOVE #field_name'      ,
+                        ExpressionAttributeNames = {'#field_name': field_name},
+                        ReturnValues             = 'ALL_NEW'              )
+
+        result        = self.client().update_item(**kwargs)
+        raw_document  = result.get('Attributes')
+        self.document = self.table.dynamo_db.document_deserialize(raw_document)
+        return True
--- tests/integration/aws/dynamo_db/models/test_DyDB__Document.py
+++ tests/integration/aws/dynamo_db/models/test_DyDB__Document.py
@@ -1,8 +1,9 @@
-
+from osbot_aws.aws.boto3.Capture_Boto3_Error import capture_boto3_error
 from osbot_aws.aws.dynamo_db.domains.DyDB__Table import DyDB__Table
 from osbot_aws.aws.dynamo_db.models.DyDB__Document import DyDB__Document
 from osbot_aws.testing.TestCase__Dynamo_DB import TestCase__Dynamo_DB
 from osbot_utils.utils.Dev import pprint
+from osbot_utils.utils.Json import to_json_str
 from osbot_utils.utils.Misc import is_guid, random_text
 
 
@@ -61,17 +62,54 @@ class test_DyDB__Document(TestCase__Dynamo_DB):
             assert dydb_document.document == self.dydb_document.document
             assert dydb_document.json()   == self.dydb_document.json()
 
+    def test___repr__(self):
+        assert repr(self.dydb_document) == f'[DyDB__Document]\n' + to_json_str(self.dydb_document.document)
+
+    def test_deserialize_value(self):
+        value     = {"an_str": 'here', 'an_int': 42, "some_bytes": b"are there", "an_dict": {"is": "here"}}
+        value_raw = {'M': {'an_dict'   : {'M': {'is': {'S': 'here' }}},
+                           'an_int'    : {'N': '42'                  },
+                           'an_str'    : {'S': 'here'                },
+                           'some_bytes': {'B': b'are there'          }}}
+        with self.dydb_document as _:
+            assert _.serialize_value  (value                         ) == value_raw
+            assert _.deserialize_value(value_raw                     ) == value
+            assert _.serialize_value  (_.deserialize_value(value_raw)) == value_raw
+            assert _.deserialize_value(_.serialize_value  (value    )) == value
+
+    #
+
     def test_add_field(self):
         with self.dydb_document as _:
             assert _.fields() == ['an_str', 'answer', 'id', 'something_random']
             new_field  = 'something_new'
             new_value  = 'with a new value'
             add_result = _.add_field(new_field, new_value)
-            assert add_result == {'Attributes': {new_field: {'S': new_value}}}
+            assert add_result == {new_field: {'S': new_value}}
             assert _.fields() == ['an_str', 'answer', 'id', 'something_random']
 
             assert _.reload() == {**self.document, new_field:new_value}
             assert _.fields() == ['an_str', 'answer', 'id', new_field, 'something_random']
+
+            assert _.document != self.document
+            assert _.document == {**self.document, new_field:new_value}
+
+            assert _.delete_field(new_field) is True
+            assert _.fields()                == ['an_str', 'answer', 'id', 'something_random']
+            assert _.document                == self.document
+            assert _.delete_field(new_field) is False
+
+    @capture_boto3_error
+    def test_add_to_list(self):
+        with self.dydb_document as _:
+            assert _.fields() == ['an_str', 'answer', 'id', 'something_random']
+            new_list = 'a_list'
+            new_value_1 = 'the answer'
+            new_value_2 = 'is 42'
+            assert _.add_to_list(new_list, new_value_1) == {'a_list': {'L': [{'S': 'the answer'}]}}
+
+
+
 
 
 
