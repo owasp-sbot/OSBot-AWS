c88f5dc06a2f7b4631f66153af904e20cbd46ed3
Dinis Cruz <dinis.cruz@owasp.org>
2024-06-04 21:43:30 +0100

    added class EC__Create__AMI that creates AMIs :)

---
+++ tests/integration/aws/ec2/utils/test_EC2__Create_AMI.py
@@ -0,0 +1,23 @@
+import pytest
+
+from osbot_aws.aws.ec2.EC2_Instance import EC2_Instance
+from osbot_aws.aws.ec2.utils.EC2__Create__AMI   import EC2__Create__AMI
+from osbot_aws.aws.ec2.utils.TestCase__EC2      import TestCase__EC2
+from osbot_utils.decorators.methods.capture_exception import capture_exception
+from osbot_utils.utils.Dev                      import pprint
+
+
+@pytest.mark.skip("only to be executed from the CI pipeline")
+class test_EC2__Create__AMI(TestCase__EC2):
+
+    def setUp(self):
+        self.ec2_create_ami = EC2__Create__AMI(ec2=self.ec2)
+        self.ec2_create_ami.ec2_create_instance.ec2 = self.ec2
+
+    def test_create_instance(self):
+        instance_id = self.ec2_create_ami.create_instance()
+        pprint(instance_id)
+
+    @capture_exception
+    def test_create_ami__for__osbot_utils(self):
+        self.ec2_create_ami.create_ami__for__osbot_utils()       # todo , add workflow to wait for the ami to be created and then stop the instance
