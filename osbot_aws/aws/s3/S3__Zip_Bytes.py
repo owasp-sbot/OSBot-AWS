9182ba7fd27018d94ce5ac6b32f8e5958fecb653
Dinis Cruz <dinis.cruz@owasp.org>
2024-06-17 16:32:04 +0100

    added method to save zip files to S3

--- osbot_aws/aws/s3/S3__Zip_Bytes.py
+++ osbot_aws/aws/s3/S3__Zip_Bytes.py
@@ -4,3 +4,9 @@ from osbot_utils.helpers.Zip_Bytes import Zip_Bytes
 
 class S3__Zip_Bytes(Zip_Bytes):
     s3: S3
+
+    def save_to_s3(self, s3_bucket, s3_key):
+        kwargs = dict(file_bytes = self.zip_bytes,
+                      bucket     = s3_bucket     ,
+                      key        = s3_key        )
+        return self.s3.file_create_from_bytes(**kwargs)
--- tests/unit/aws/s3/test_S3__Zip_Bytes.py
+++ tests/unit/aws/s3/test_S3__Zip_Bytes.py
@@ -5,32 +5,37 @@ import boto3
 from osbot_aws.AWS_Config import AWS_Config
 from osbot_aws.aws.boto3.View_Boto3_Rest_Calls import print_boto3_calls
 from osbot_aws.aws.iam.IAM import IAM
+from osbot_aws.aws.s3.S3__On_Temp_Bucket import S3__On_Temp_Bucket
 from osbot_aws.aws.s3.S3__Zip_Bytes                     import S3__Zip_Bytes
 from osbot_aws.aws.sts.STS import STS
 from osbot_aws.testing.TestCase__S3                     import TestCase__S3
 from osbot_utils.decorators.methods.capture_exception   import capture_exception
 from osbot_utils.utils.Dev                              import pprint
 from osbot_utils.utils.Objects import obj_info
+from osbot_utils.utils.Zip import zip_bytes__files
 
 
 class test_S3__Zip_Bytes(TestCase__S3):
 
 
     #@print_boto3_calls()
-    def test_buckets(self):
+    def test_save_to_s3(self):
+        s3_on_temp_bucket =  S3__On_Temp_Bucket()
+        bucket_name       = s3_on_temp_bucket.bucket_name()
+        s3_key            = 'test_save_to_s3.zip'
         with S3__Zip_Bytes(s3=self.s3) as _:
-            pprint(AWS_Config().region_name())
-            return
-            #pprint(_.s3.buckets())
-            s3_client = _.s3.client()
-            #obj_info(_.s3.client().meta)
-            pprint(_.s3.sts().caller_identity())
-            #pprint(STS().caller_identity())
-            role_arn = 'arn:aws:sts::381492182978:assumed-role/osbot__temp_role_for__test_S3/osbot_sts_session__brmwdja'
-            role_name = 'osbot__temp_role_for__test_S3'
-            #iam_client = _.s3.iam_assume_role().boto3_client('iam')
-            #pprint(iam_client.list_attached_role_policies(RoleName=role_name))
-            pprint(IAM(role_name=role_name).role_policies())
+            _.add_random_file()
+            assert _.size() == 1
+            assert _.save_to_s3(bucket_name, s3_key    ) is True
+
+            assert _.s3.file_exists(bucket_name, s3_key) is True
+            zip_bytes_from_s3 = _.s3.file_bytes(bucket_name, s3_key)
+            assert zip_bytes_from_s3 == _.zip_bytes
+            assert zip_bytes__files(zip_bytes_from_s3) == _.files()
+            assert _.s3.file_delete(bucket_name, s3_key) is True
+
+
+
 
 
 
