e82769351a6c92cf1653448b6ac1c8cd9b2f0ad0
Dinis Cruz <dinis.cruz@owasp.org>
2024-12-28 00:21:33 +0000

    added CloudFront invalidate_paths method

--- osbot_aws/aws/cloud_front/Cloud_Front.py
+++ osbot_aws/aws/cloud_front/Cloud_Front.py
@@ -1,7 +1,8 @@
-from osbot_aws.apis.Session import Session
-
-from osbot_utils.base_classes.Kwargs_To_Self import Kwargs_To_Self
-
+import time
+from typing                                     import List
+from osbot_utils.decorators.methods.type_safe   import type_safe
+from osbot_aws.apis.Session                     import Session
+from osbot_utils.base_classes.Kwargs_To_Self    import Kwargs_To_Self
 
 class Cloud_Front(Kwargs_To_Self):
 
@@ -14,3 +15,14 @@ class Cloud_Front(Kwargs_To_Self):
         items = response.get('DistributionList', {}).get('Items', [])
         return items
 
+    def invalidate_path(self, distribution_id: str, paths: str):
+        return self.invalidate_paths(distribution_id, [paths])
+
+    @type_safe
+    def invalidate_paths(self, distribution_id:str, paths: List[str]):
+        kwargs = dict(DistributionId    = distribution_id,
+                      InvalidationBatch = { 'Paths'          : { 'Quantity': len(paths)            ,
+                                                                 'Items'   : paths                 },
+                                            'CallerReference': f'invalidation-{int(time.time()) }' })  # Unique reference
+        response = self.client().create_invalidation(**kwargs)
+        return response
