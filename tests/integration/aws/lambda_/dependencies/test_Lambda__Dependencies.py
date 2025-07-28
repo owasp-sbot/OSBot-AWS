35350df3f7707f2563f926823c572a9dd747d539
Dinis Cruz <dinis.cruz@owasp.org>
2025-07-28 17:24:53 +0100

    removed non-used class and minor fix

--- tests/integration/aws/lambda_/dependencies/test_Lambda__Dependencies.py
+++ tests/integration/aws/lambda_/dependencies/test_Lambda__Dependencies.py
@@ -1,6 +1,5 @@
 from unittest                                                           import TestCase
 from osbot_aws.aws.lambda_.dependencies.Lambda__Dependencies            import Lambda__Dependencies
-from osbot_aws.aws.lambda_.dependencies.Lambda__Dependencies__Storage   import Lambda__Dependencies__Storage
 
 
 class test_Lambda__Dependencies(TestCase):
@@ -12,4 +11,3 @@ class test_Lambda__Dependencies(TestCase):
     def test__init__(self):
         with self.lambda_dependencies as _:
             assert type(_                     ) is Lambda__Dependencies
-            assert type(_.dependencies_storage) is Lambda__Dependencies__Storage
