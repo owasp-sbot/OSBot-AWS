c138503e90eb4cd0a8c9960b922ab52ca50cb4b3
Dinis Cruz <dinis.cruz@owasp.org>
2025-08-14 15:59:59 +0100

    another fix

--- tests/integration/aws/lambda_/dependencies/test_Lambda__Dependency__Inside_Lambda.py
+++ tests/integration/aws/lambda_/dependencies/test_Lambda__Dependency__Inside_Lambda.py
@@ -268,8 +268,8 @@ class test_Lambda__Dependency__Inside_Lambda(TestCase):
             with Deploy_Lambda(handler=run) as _:
                 _.add_file(file_to_add)
                 _.add_file     (lambda_handler)
-                assert _.files() == [ '/boto3__lambda.py'    ,
-                                      '/test_Lambda__Dependency__Inside_Lambda.py']
+                assert _.files() == sorted([ '/boto3__lambda.py'    ,
+                                             '/test_Lambda__Dependency__Inside_Lambda.py'])
                 assert _.deploy() is True
                 assert _.invoke() == boto3__lambda.ping() == 'pong'
                 assert _.info().get('Configuration').get('CodeSize') < 1300                                              # confirm that size of the code uploaded is still very small
