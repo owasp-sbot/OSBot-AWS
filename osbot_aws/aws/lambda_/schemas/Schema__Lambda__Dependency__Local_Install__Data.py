00270fed9921c02195b3ce28c75a32cc9a1905ff
Dinis Cruz <dinis.cruz@owasp.org>
2025-09-19 14:13:41 +0100

    updated to latest version of osbot-utils

--- osbot_aws/aws/lambda_/schemas/Schema__Lambda__Dependency__Local_Install__Data.py
+++ osbot_aws/aws/lambda_/schemas/Schema__Lambda__Dependency__Local_Install__Data.py
@@ -2,7 +2,7 @@ from typing                                                                     
 from osbot_utils.type_safe.Type_Safe                                                import Type_Safe
 from osbot_aws.aws.lambda_.schemas.Safe_Str__File__Path__Python_Package             import Safe_Str__File__Path__Python_Package
 from osbot_aws.aws.lambda_.schemas.Safe_Str__File__Name__Python_Package             import Safe_Str__File__Name__Python_Package
-from osbot_utils.type_safe.primitives.domains.identifiers.Timestamp_Now             import Timestamp_Now
+from osbot_utils.type_safe.primitives.domains.identifiers.safe_int.Timestamp_Now             import Timestamp_Now
 from osbot_utils.type_safe.primitives.domains.files.safe_str.Safe_Str__File__Path   import Safe_Str__File__Path
 
 class Schema__Lambda__Dependency__Local_Install__Data(Type_Safe):
